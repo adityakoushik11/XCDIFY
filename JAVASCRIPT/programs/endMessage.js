442f2e823b918dc68f1c9a9086d53518e2350d05
aditya <aditya.koushik11@gmail.com>
2017-04-07 17:50:39 +0530

    node

---
+++ JAVASCRIPT/programs/endMessage.js
@@ -0,0 +1,2 @@
+var chalk=require("chalk");
+console.log(chalk.yellow("ending the environment in development mode"));
---
+++ JAVASCRIPT/programs/startMessage.js
@@ -0,0 +1,2 @@
+var chalk=require("chalk");
+console.log(chalk.green("starting the environment in development mode ..."));
